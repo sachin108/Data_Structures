aa152f714295b30f023c6efd16fab9e7233e96e4
sachin108 <88074105+sachin108@users.noreply.github.com>
2021-09-01 22:50:34 +0530

    Create Rotate_List.java

---
+++ Linked_List/Rotate_List.java
@@ -0,0 +1,58 @@
+import java.util.*;
+public class Rotate_List{
+	static Node head,tail;
+	
+	static void addNode(int data){
+		Node newNode=new Node(data);
+		if(head==null){
+			head=newNode;
+			tail=newNode;
+		}
+		else{
+			tail.next=newNode;
+			tail=newNode;
+		}
+		tail.next=null;
+	}
+	
+	static void rotate(int rotateBY){
+		Node current=head;
+		int counter=1;
+		while(current!=null && counter<rotateBY){
+			current=current.next;
+			++counter;
+		}
+		Node new_Head=current;
+		while(current.next!=null)
+			current=current.next;
+		current.next=head; // last element of Linked List is pointing to head (previous)
+		head=new_Head.next;	// new head is new_Head
+		new_Head.next=null;	// after above 2 operations list is circular so break the chain by assigning null
+	}
+	
+	static void traverse(Node x){
+		Node current=x;
+		while(current!=null){
+			System.out.println(current.data+"	");
+			current=current.next;
+		}
+	}
+	
+	public static void main(String[] args){
+		for(int i=1;i<7;i++){
+			addNode(i);
+		}
+		System.out.println("before rotating Linked List");
+		traverse(head);
+		System.out.println("After rotating Linked List");
+		rotate(2);
+		traverse(head);
+	}
+}
+class Node{
+	int data;
+	Node next;
+	Node(int data){
+		this.data=data;
+	}
+}
