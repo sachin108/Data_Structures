a2671e58dadf0fc64c1a1cca5c56a0d128c693e6
sachin108 <sachin108rajput@hotmail.com>
2021-11-16 10:36:02 +0530

    updated

--- Stack/StackX.java
+++ Stack/StackX.java
@@ -1,3 +1,5 @@
+import java.util.EmptyStackException;
+
 public class StackX{
     private ListNode top;
     private int length;
@@ -12,8 +14,52 @@ public class StackX{
         }
     }
 
+    public StackX(){
+        top=null;
+        length=0;
+    }
+
+    public int length(){
+        return length;
+    }
+
+    public boolean isEmpty(){
+        return length==0;
+    }
+
     public void push(int data){
         ListNode temp=new ListNode(data);
+        temp.next=top;
+        top=temp;
+        ++length;
+    }
 
+    public int peek(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        return top.data;
+    }
+
+    public int pop(){
+        if(isEmpty())
+            throw new EmptyStackException();
+        int result=top.data;
+        top=top.next;
+        --length;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        StackX stack=new StackX();
+        for (int i = 0; i < 7; i++) {
+            stack.push(i);
+        }
+        System.out.println(stack.peek());
+        stack.pop();
+        System.out.println(stack.peek());
+        stack.pop();
+        System.out.println(stack.peek());
+        stack.pop();
     }
 }
